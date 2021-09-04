d5f7fe2b4e7908e8e6f6d78f8068058b2e958088
Rinori99 <rinori99@live.com>
2021-09-04 18:43:44 +0200

    login by pressing enter

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import '../style/auth.css';
 import '../style/style.css';
@@ -8,6 +8,17 @@ function LoginComponent() {
 
     const [shouldShowWrongCredentials, setShouldShowWrongCredentials] = useState(false);
     const history = useHistory();
+
+    useEffect(() => {
+        const clickLoginOnEnterPress = (event) => {
+            if (event.keyCode == 13) {
+                document.getElementById('login-btn').click();
+            }
+        };
+
+        document.getElementById("email-field").addEventListener("keyup", clickLoginOnEnterPress);
+        document.getElementById("password-field").addEventListener("keyup", clickLoginOnEnterPress);
+    }, []);
     
     const handleLoginClick = () => {
         const email = document.getElementById("email-field").value;
@@ -40,7 +51,7 @@ function LoginComponent() {
                 <h2 className="form-signin-heading">Please login</h2>
                 <input id="email-field" type="text" className="form-control sn-text-box" placeholder="Email Address" />
                 <input id="password-field" type="password" className="form-control sn-text-box" placeholder="Password"/>
-                <button className="form-login-btn sn-btn" onClick={ handleLoginClick }>Login</button>
+                <button id="login-btn" className="form-login-btn sn-btn" onClick={ handleLoginClick } type="submit">Login</button>
                 { shouldShowWrongCredentials ? (<h4 className="wrong-auth">Wrong email or password, please try again!</h4>) : ""}
             </div>
         </div>) : (<Redirect to="/browse/home"></Redirect>)
