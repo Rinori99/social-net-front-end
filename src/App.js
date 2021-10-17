0a708a8cd285d233b4668d972fece9e9486477ce
Rinori99 <rinori99@live.com>
2021-10-17 20:09:27 +0200

    fixed profile file name

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Login from './components/Login';
 import Signup from './components/Signup';
-import Profile from './components/Profile';
+import Profile from './components/profile';
 import Navigation from './components/Navigation';
 import { Redirect } from 'react-router-dom/cjs/react-router-dom';
 
